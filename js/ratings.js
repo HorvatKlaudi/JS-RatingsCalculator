470750a6751095c0633d37b044b946452908ee0f
Klaudia Horvat <klaudia.mihajlova3b@gmail.com>
2022-04-19 15:17:28 +0200

    second commit

--- js/ratings.js
+++ js/ratings.js
@@ -2,6 +2,6 @@ function collect_ratings(){
     let rating = 0;
     const ratings = {count: 0, sum: 0, average: 0};
 
-    const elements = document.querySelectorAll("ratings")
+    const elements = document.querySelectorAll("rating")
 
 }
